92e25abd1e387be07949661d0941ca61c819a065
Daniela Martínez <danymb17@gmail.com>
2022-12-27 12:42:32 -0600

    Cambios

--- Proyecto/src/clases/JugadorPortero.java
+++ Proyecto/src/clases/JugadorPortero.java
@@ -3,14 +3,16 @@ package clases;
 public class JugadorPortero extends Futbolista{
 	
 	private float altura;
+	private int reflejos;
 	
 	public JugadorPortero() {
 		System.out.println("Se crea el objeto de Jugador Campo");
 	}
-	public JugadorPortero(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, float altura) {
-		super(nombre,nacionalidad,fechaNacimiento,numero);
+	public JugadorPortero(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, String valorActual, String perfil, float altura, int reflejos) {
+		super(nombre,nacionalidad,fechaNacimiento,numero,valorActual,perfil);
 		System.out.println("Se crea el objeto de Jugador Portero");
 		this.altura=altura; 
+		this.reflejos=reflejos;
 	}
 	
 	public float getAltura() {
@@ -19,11 +21,20 @@ public class JugadorPortero extends Futbolista{
 	public void setAltura(float altura) {
 		this.altura=altura;
 	}
+
+	public float getReflejos() {
+		return reflejos;
+	}
+	public void setReflejos(int reflejos) {
+		this.reflejos=reflejos;
+	}
 	
 	public void leerDatos() {
 		super.LeerDatos();
 		System.out.println("Ingrese la altura del portero (cm): ");
 		this.altura=lee.nextFloat();
+		System.out.println("Ingrese la calificacion de reflejos del portero (0-100): ");
+		this.reflejos=lee.nextInt();
 		
 	}
 
@@ -32,7 +43,10 @@ public class JugadorPortero extends Futbolista{
 		System.out.println("Nacionalidad: "+this.getNacionalidad());
 		System.out.println("Fecha de nacimiento: "+this.getFechaNacimiento());
 		System.out.println("Numero: "+this.getNumero());
-		System.out.println("Altura: "+this.getAltura()+"\n");
+		System.out.println("Valor actual: "+this.getValorActual());
+		System.out.println("Pierna preferida: "+this.getPefil());
+		System.out.println("Altura: "+this.getAltura());
+		System.out.println("Reflejos: "+this.getReflejos()+"\n");
 		
 	}
 	
