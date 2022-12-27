51364812b1f5f65a8f8530780b91d79618a31041
Daniela Martínez <danymb17@gmail.com>
2022-12-27 14:29:44 -0600

    Update JugadorCampo.java

--- JugadorCampo.java
+++ JugadorCampo.java
@@ -1,19 +1,24 @@
 import gui.Interfaz;
 
+
 public class JugadorCampo extends Futbolista{
 	private String posicion;
 	private int fuerzaTiro;
+	private int precisionPases;
+	private int estadoFisico;
 	
 	public JugadorCampo() {
 		System.out.println("Se crea el objeto de Jugador Campo");
 		mostrarVentana();
 	}
 
-	public JugadorCampo(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, String valorActual, String perfil, String posicion, int fuerzaTiro ) {
+	public JugadorCampo(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, String valorActual, String perfil, String posicion, int fuerzaTiro, int precisionPases, int estadoFisico ) {
 		super(nombre,nacionalidad,fechaNacimiento,numero,valorActual,perfil);
 		System.out.println("Se crea el objeto de Jugador Campo");
 		this.posicion=posicion; 
 		this.fuerzaTiro=fuerzaTiro;
+		this.precisionPases=precisionPases;
+		this.estadoFisico=estadoFisico;
 	}
 	
 	
@@ -31,12 +36,31 @@ public class JugadorCampo extends Futbolista{
 		this.fuerzaTiro=fuerzaTiro;
 	}
 
+	public int getPrecisionPases() {
+		return precisionPases;
+	}
+	public void setPrecisionPases(int precisionPases) {
+		this.precisionPases=precisionPases;
+	}
+
+	public int getEstadoFisico() {
+		return estadoFisico;
+	}
+	public void setEstadOFisico(int estadoFisico) {
+		this.estadoFisico=estadoFisico;
+	}
+
+
 	public void leerDatos() {
 		super.LeerDatos();
 		System.out.println("Ingrese posicion del jugador (Defensa, Centrocampista, Delantero): ");
 		this.posicion=lee.nextLine();
 		System.out.println("Ingrese la fuerza de tiro del jugador: ");
 		this.fuerzaTiro=lee.nextInt();
+		System.out.println("Ingrese la precision de pases del portero (0-100): ");
+		this.precisionPases=lee.nextInt();
+		System.out.println("Ingrese el estado fisico del portero (0-100): ");
+		this.estadoFisico=lee.nextInt();
 		
 	}
 
@@ -48,7 +72,9 @@ public class JugadorCampo extends Futbolista{
 		System.out.println("Valor actual: "+this.getValorActual());
 		System.out.println("Pierna preferida: "+this.getPefil());
 		System.out.println("Posicion: "+this.getPosicion());
-		System.out.println("Fuerza de Tiro: "+this.getFuerzaTiro()+"\n");
+		System.out.println("Fuerza de Tiro: "+this.getFuerzaTiro());
+		System.out.println("Precision de pases: "+this.getPrecisionPases());
+		System.out.println("Estado fisico: "+this.getEstadoFisico()+"\n");
 	}
 	
 	public void mostrarVentana() {
