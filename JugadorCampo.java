822f91603d511bdeb2ddb76d743690f93e8ca856
Daniela Martínez <danymb17@gmail.com>
2022-12-27 13:09:23 -0600

    Update JugadorCampo.java

--- JugadorCampo.java
+++ JugadorCampo.java
@@ -1,16 +1,20 @@
+package clases;
 import gui.Interfaz;
 
 public class JugadorCampo extends Futbolista{
 	private String posicion;
+	private int fuerzaTiro;
 	
 	public JugadorCampo() {
 		System.out.println("Se crea el objeto de Jugador Campo");
 		mostrarVentana();
 	}
-	public JugadorCampo(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, String posicion) {
-		super(nombre,nacionalidad,fechaNacimiento,numero);
+
+	public JugadorCampo(String nombre, String nacionalidad, Fecha fechaNacimiento, int numero, String valorActual, String perfil, String posicion, int fuerzaTiro ) {
+		super(nombre,nacionalidad,fechaNacimiento,numero,valorActual,perfil);
 		System.out.println("Se crea el objeto de Jugador Campo");
 		this.posicion=posicion; 
+		this.fuerzaTiro=fuerzaTiro;
 	}
 	
 	
@@ -21,10 +25,19 @@ public class JugadorCampo extends Futbolista{
 		this.posicion=posicion;
 	}
 	
+	public int getFuerzaTiro() {
+		return fuerzaTiro;
+	}
+	public void setFuerzaTiro(int fuerzaTiro) {
+		this.fuerzaTiro=fuerzaTiro;
+	}
+
 	public void leerDatos() {
 		super.LeerDatos();
 		System.out.println("Ingrese posicion del jugador (Defensa, Centrocampista, Delantero): ");
 		this.posicion=lee.nextLine();
+		System.out.println("Ingrese la fuerza de tiro del jugador: ");
+		this.fuerzaTiro=lee.nextInt();
 		
 	}
 
@@ -33,7 +46,10 @@ public class JugadorCampo extends Futbolista{
 		System.out.println("Nacionalidad: "+this.getNacionalidad());
 		System.out.println("Fecha de nacimiento: "+this.getFechaNacimiento());
 		System.out.println("Numero: "+this.getNumero());
-		System.out.println("Posicion: "+this.getPosicion()+"\n");
+		System.out.println("Valor actual: "+this.getValorActual());
+		System.out.println("Pierna preferida: "+this.getPefil());
+		System.out.println("Posicion: "+this.getPosicion());
+		System.out.println("Fuerza de Tiro: "+this.getFuerzaTiro()+"\n");
 	}
 	
 	public void mostrarVentana() {
